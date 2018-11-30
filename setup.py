eff4d134323a06e7ca0c7bbd6379806fbc024e91
WO <wo@kotocoin.info>
2018-11-30 17:06:18 +0900

    Update setup.py for PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ yescrypt_module = Extension('yescrypt',
                             extra_compile_args=['-O2', '-funroll-loops', '-fomit-frame-pointer'],
                             include_dirs=['.'])
 
-setup (name = 'yescrypt',
-       version = '1.1',
-       description = 'Bindings for yescrypt proof of work',
+setup (name = 'yescrypt_koto',
+       version = '1.2',
+       description = 'Bindings for yescrypt proof of work for Koto',
+       long_description = 'Bindings for yescrypt proof of work for Koto',
+       author = "WO",
+       author_email = "wo@kotocoin.info",
+       url='https://github.com/wo01/yescrypt_python/',
        ext_modules = [yescrypt_module])
